Refactored and fixed scaling issues

main.py
@@ -8,30 +8,39 @@ import utils
 def main(file_path):
 
     df = pd.read_csv(file_path+'subject-info.csv', sep=';')
+    print(f'Num of cols in original df: {len(df.columns)}, Num of subjects in original df: {len(df)}')
+    # df.to_csv('original_df.csv', index=False)
 
     cleaned_df = preprocess_data.cleaning(df)
-    df_after_one_hot = preprocess_data.columns_to_one_hot(cleaned_df)
-    imputed_df = preprocess_data.impute_missing_values(df_after_one_hot)
+    print(f'Num of cols after cleaning: {len(cleaned_df.columns)}, Num of subjects after cleaning: {len(cleaned_df)}')
+    cleaned_df.to_csv('cleaned_df.csv', index=False)
+
+    df_after_one_hot, continuous_features, categorical_features = preprocess_data.columns_to_one_hot(cleaned_df)
+    print(f'Num of cols after one hot encoding: {len(df_after_one_hot.columns)}, Num of subjects after one hot encoding: {len(df_after_one_hot)}')
+    df_after_one_hot.to_csv('df_after_one_hot.csv', index=False)
+
+    continuous_features_imputed_df, categorical_features_imputed_df = preprocess_data.impute_missing_values(continuous_features, categorical_features)
+    continuous_features_imputed_df.to_csv('continuous_features_imputed_df.csv', index=False)
+    categorical_features_imputed_df.to_csv('categorical_features_imputed_df.csv', index=False)
+
+    scaled_df = preprocess_data.standard_scalar(continuous_features_imputed_df, categorical_features_imputed_df)
+    preprocessed_df = scaled_df
     
-
-    # Total missing after KNN imputation, should be 0
-    print('Missing: %d' % sum(np.isnan(imputed_df).flatten()))
-
-    preprocessed_df = pd.DataFrame(imputed_df, columns=df_after_one_hot.columns)
     preprocessed_df.to_csv('preprocessed_data.csv', index=False)
 
     features = preprocessed_df.drop('Cause of death', axis=1)
-    one_hot_labels = utils.load_labels(file_path)
+    one_hot_labels = preprocess_data.load_labels(file_path)
 
-    n_comps = None
-    # explained_variance_ratio, eigenvals, pcs, pca_features = utils.pca(features, num_components=n_comps, plot_bool=False)
- 
-    # print(f"PCA Features shape: {pca_features.shape}")
-    # print("Explained Variance Ratio:", explained_variance_ratio)  # Variance explained by each component
-    # print(f'Explained Variance Ratio Sum: {sum(explained_variance_ratio)}')
+    n_comps = 5
 
-    num_epochs = 2
-    cnn_average_test_AUC, cnn_history = train_model.train_k_fold(features, one_hot_labels, num_epochs=num_epochs)
+    explained_variance_ratio, eigenvals, pcs, pca_features = utils.pca(features, num_components=n_comps, plot_bool=False)
+    print(pca_features)
+    print(f"PCA Features shape: {pca_features.shape}")
+    print("Explained Variance Ratio:", explained_variance_ratio)  # Variance explained by each component
+    print(f'Explained Variance Ratio Sum: {sum(explained_variance_ratio)}')
+
+    num_epochs = 10
+    cnn_average_test_AUC, cnn_history = train_model.train_k_fold(pca_features, one_hot_labels, num_epochs=num_epochs)
 
     print(f"Average CNN Test AUC: {cnn_average_test_AUC}")
     utils.plot_metrics(model_history=cnn_history, metrics_plot_name=f'cnn_metrics_{n_comps}_components', model_name='CNN')

preprocess_data.py
@@ -1,8 +1,20 @@
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
 
 def cleaning(df):
+
+    # Exclude subjects who were catgeorized as 'Lost to follow-up':
+    df = df.loc[df['Exit of the study'] != 1]
+    df.to_csv('after_dropping_subs_df.csv', index=False)
+
+    # Drop 'Exit of the study' column since 'Cause of death' is the target variable:
+    df = df.drop('Exit of the study', axis=1)
+
     # Convert 'Holster Onset' column to seconds since it's original format is in 'HH:MM:SS':
     val_arr = []
     for val in df['Holter onset (hh:mm:ss)']:
@@ -17,96 +29,174 @@ def cleaning(df):
   
     df['Holter onset (hh:mm:ss)'] = val_arr
 
-    df['Number of ventricular premature contractions per hour'] = df['Number of ventricular premature contractions per hour'].replace(",", ".", regex=True)
-    
     df = df.replace(",", ".", regex=True)
 
     nans_removed_df = df.dropna(axis=1, how='all') 
 
-    # Drop Patient ID column since row values are strings (KNN needs numerical values)
-    dropped_patient_id = nans_removed_df.drop('Patient ID', axis=1)
+    # Drop Patient ID column as it's not a feature
+    # Drop 'Number of ventricular premature contractions per hour' due to nonsensical values
+    # Drop 'cigarettes /year' due to unrealistic, inflated values
+    # Drop 'Hig-resolution ECG available' and 'Holter available' since they are not features
+    columns_to_be_dropped = ['Patient ID', 'Number of ventricular premature contractions per hour', 'cigarettes /year', 'Hig-resolution ECG available', 'Holter available']
 
-    # Remove columns with nonsensical or inflated values: 
-    removed_inflated_df = dropped_patient_id.drop('cigarettes /year', axis=1) # values are too high to be realistic
+    df_columns_removed = nans_removed_df.drop(columns_to_be_dropped, axis=1)
+    
+    # Replace age >89 with age 90: 
+    df_columns_removed['Age'] = df_columns_removed['Age'].replace('>89', 90)
 
-    # Remove range of ages above 89
-    for index, val in enumerate(removed_inflated_df['Age']):
-        try:
-            age = val
-            if age == '>89':
-                removed_inflated_df.drop(index, inplace=True)
-            else:
-                age = int(val)
-        except ValueError:
-            print(f"ValueError: {index, val}")
-            pass
+    return df_columns_removed
 
-    return removed_inflated_df
 
 def columns_to_one_hot(df):
 
     # Specify the columns you want to one-hot encode
     columns_to_encode = [
-        'Exit of the study', 
+        'Calcium channel blocker (yes=1)',
+        'SCD_4years SinusRhythm',
+        'Q-waves (necrosis, yes=1)',
+        'QRS > 120 ms ',
+        'HF_4years SinusRhythm',
+        'Gender (male=1)',
+        'NYHA class',
+        'Diabetes (yes=1)',
+        'History of dyslipemia (yes=1)',
+        'Peripheral vascular disease (yes=1)',
+        'History of hypertension (yes=1)',
+        'Prior Myocardial Infarction (yes=1)',
+        'Signs of pulmonary venous hypertension (yes=1)',
+        'Right ventricle contractility (altered=1)',
+        'Left ventricular hypertrophy (yes=1)',
+        'Non-sustained ventricular tachycardia (CH>10)',
+        'Diabetes medication (yes=1)',
+        'Amiodarone (yes=1)',
+        'Angiotensin-II receptor blocker (yes=1)',
+        'Anticoagulants/antitrombotics  (yes=1)',
+        'Betablockers (yes=1)',
+        'Digoxin (yes=1)',
+        'Loop diuretics (yes=1)',
+        'Spironolactone (yes=1)',
+        'Statins (yes=1)',
+        'Hidralazina (yes=1)',
+        'ACE inhibitor (yes=1)',
+        'Nitrovasodilator (yes=1)',
         'HF etiology - Diagnosis', 
         'Prior implantable device', 
         'Prior Revascularization', 
         'Syncope', 
-        'Mitral valve insufficiency ',  # COLUMN NAME INCLUDES SPACE 
+        'Mitral valve insufficiency ',
         'Mitral flow pattern', 
-        'ECG rhythm ', # COLUMN NAME INCLUDES SPACE 
+        'ECG rhythm ',
         'Intraventricular conduction disorder', 
-        'Holter  rhythm ', # COLUMN NAME INCLUDES SPACE 
+        'Holter  rhythm ',
         'Ventricular Extrasystole', 
         'Ventricular Tachycardia', 
         'Paroxysmal supraventricular tachyarrhythmia', 
-        'Bradycardia'
+        'Bradycardia' 
     ]
 
-    # One-hot encode the specified columns
-    X_dropped_dummies = pd.get_dummies(df, columns=columns_to_encode, drop_first=True)
+    binary_categorical_columns = []
+    one_hot_encoded_columns = []
 
-    for col in X_dropped_dummies.select_dtypes(include=['bool']).columns:
-        X_dropped_dummies[col] = X_dropped_dummies[col].astype(int)
+    # Process binary categorical features
+    for col in columns_to_encode:
+        # Check if the column has binary values (0 or 1)
+        if df[col].isin([0, 1]).all():
+            # Create two separate columns: (True) and (False)
+            true_col = f'{col} (True)'
+            false_col = f'{col} (False)'
+            df[true_col] = (df[col] == 1).astype(int)
+            df[false_col] = (df[col] == 0).astype(int)
+            
+            # Drop the original binary column
+            df.drop(columns=[col], inplace=True)
+            
+            # Track new binary columns
+            binary_categorical_columns.extend([true_col, false_col])
+        else:
+            # Keep non-binary categorical columns for one-hot encoding
+            one_hot_encoded_columns.append(col)
 
-    return X_dropped_dummies
+    # Perform one-hot encoding for remaining categorical columns
+    df_after_one_hot = pd.get_dummies(df, columns=one_hot_encoded_columns, drop_first=True)
+    df_after_one_hot.to_csv('df_after_one_hot_before_booling.csv', index=False)
+
+    # Capture new one-hot-encoded column names
+    one_hot_columns = [col for col in df_after_one_hot.columns if col not in df.columns]
+    all_categorical_columns = binary_categorical_columns + one_hot_columns
+
+    # Convert boolean columns to integers
+    for col in df_after_one_hot.select_dtypes(include=['bool']).columns:
+        df_after_one_hot[col] = df_after_one_hot[col].astype(int)
+
+    # Separate continuous and categorical features
+    continuous_features = df_after_one_hot.drop(columns=all_categorical_columns)
+    # continuous_features = df.drop(columns=columns_to_encode, errors='ignore')
+    categorical_features_df = df_after_one_hot[all_categorical_columns]
+    print(f'Number of categorical features: {len(categorical_features_df.columns)}')
+    print(f'Number of continuous features: {len(continuous_features.columns)}')
+
+    return df_after_one_hot, continuous_features, categorical_features_df
 
 
-def standard_scalar(df):
-
-    continuous_features = [
-        'Exit of the study', 
-        'HF etiology - Diagnosis', 
-        'Prior implantable device', 
-        'Prior Revascularization', 
-        'Syncope', 
-        'Mitral valve insufficiency ',  # COLUMN NAME INCLUDES SPACE 
-        'Mitral flow pattern', 
-        'ECG rhythm ', # COLUMN NAME INCLUDES SPACE 
-        'Intraventricular conduction disorder', 
-        'Holter  rhythm ', # COLUMN NAME INCLUDES SPACE 
-        'Ventricular Extrasystole', 
-        'Ventricular Tachycardia', 
-        'Paroxysmal supraventricular tachyarrhythmia', 
-        'Bradycardia'
-    ]
-
-    continuous_features_df = df[continuous_features]
-    print(f"Continuous Features: {continuous_features_df.columns}")
-    non_continuous_features_df = df.drop(columns=continuous_features)
-
+def standard_scalar(continuous_features_df, categorical_columns_df):
     scaler = StandardScaler()
     df_scaled_continuous = scaler.fit_transform(continuous_features_df)
 
-    df_scaled_continuous = pd.DataFrame(df_scaled_continuous, columns=continuous_features)
-    df_final = pd.concat([df_scaled_continuous, non_continuous_features_df.reset_index(drop=True)], axis=1)
+    df_scaled_continuous = pd.DataFrame(
+        df_scaled_continuous, 
+        columns=continuous_features_df.columns, 
+        index=continuous_features_df.index
+    )
 
-    return df_final
+    # Check indices and reset if needed
+    if not continuous_features_df.index.equals(categorical_columns_df.index):
+        print("Index mismatch detected. Resetting indices.")
+        continuous_features_df = continuous_features_df.reset_index(drop=True)
+        categorical_columns_df = categorical_columns_df.reset_index(drop=True)
+        df_scaled_continuous.index = continuous_features_df.index
+
+    # Combine the scaled continuous features and categorical features
+    combined_df = pd.concat([df_scaled_continuous, categorical_columns_df], axis=1)
+
+    print(f'Number of continuous features: {len(continuous_features_df.columns)}')
+    print(f'Number of categorical features: {len(categorical_columns_df.columns)}')
+    print(f'Total number of features in combined DataFrame: {len(combined_df.columns)}')
+
+    return combined_df
 
 
-def impute_missing_values(df):
+def impute_missing_values(continuous_features_df, categorical_columns_df):
+
     imputer = KNNImputer()
-    imputer.fit(df)
-    Xtrans = imputer.transform(df)
 
-    return Xtrans
+    continuous_features_imputed = imputer.fit_transform(continuous_features_df)
+    # Total missing after KNN imputation, should be 0
+    print('Missing: %d' % sum(np.isnan(continuous_features_imputed).flatten()))
+    continuous_features_imputed_df = pd.DataFrame(continuous_features_imputed, columns=continuous_features_df.columns)
+    
+    categorical_features_imputed = imputer.fit_transform(categorical_columns_df)
+    # Total missing after KNN imputation, should be 0
+    print('Missing: %d' % sum(np.isnan(categorical_features_imputed).flatten()))
+    categorical_features_imputed_df = pd.DataFrame(categorical_features_imputed, columns=categorical_columns_df.columns)
+
+    return continuous_features_imputed_df, categorical_features_imputed_df
+
+
+def load_labels(file_path):
+    data = pd.read_csv(file_path+'subject-info.csv', sep=';')
+
+    label_column = 'Cause of death'
+    labels = data[label_column]
+
+    for index, label in enumerate(labels):
+        if label == 7.0:
+            labels[index] = 6.0
+            
+
+    encoder = LabelEncoder()
+    encoder.fit(labels)
+    # [0, 1, 2, 3] == [survivor, non-cardiac death, SCD, Pump-Failure]
+    encoded_labels = encoder.transform(labels)
+    one_hot_labels = to_categorical(encoded_labels, num_classes=len(np.unique(encoded_labels)))
+    
+    return one_hot_labels

utils.py
@@ -1,34 +1,12 @@
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
-# import autosklearn.classification
 from lime import lime_tabular
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-
-def load_labels(file_path):
-    data = pd.read_csv(file_path+'subject-info.csv', sep=';')
-
-    label_column = 'Cause of death'
-    labels = data[label_column]
-
-    for index, label in enumerate(labels):
-        if label == 7.0:
-            labels[index] = 6.0
-            
-
-    encoder = LabelEncoder()
-    encoder.fit(labels)
-    # [0, 1, 2, 3] == [survivor, non-cardiac death, SCD, Pump-Failure]
-    encoded_labels = encoder.transform(labels)
-    one_hot_labels = to_categorical(encoded_labels, num_classes=len(np.unique(encoded_labels)))
-    
-    return one_hot_labels
 
 
 def feature_importance(X_train, X_test, feature_names, label_name, model):
@@ -55,23 +33,10 @@ def feature_importance(X_train, X_test, feature_names, label_name, model):
     return feature, importance
 
 
-def auto_sklearn(features, labels, num_epochs):
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, random_state=42)
-    automl = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=30)
-    automl.fit(X_train, y_train)
-    y_pred = automl.predict(X_test)
-    y_pred_rounded = np.round(y_pred)
-    test_accuracy = accuracy_score(y_test, y_pred_rounded)
-
-    return test_accuracy, automl
-
-
 def pca(X, num_components, plot_bool):
 
-    X_scaled = standard_scalar(X)
-
-    pca = PCA(n_components=num_components)  # Reduce to 2 components
-    X_pca = pca.fit_transform(X_scaled)
+    pca = PCA(n_components=num_components)
+    X_pca = pca.fit_transform(X)
 
     if plot_bool == True:
         plt.figure(figsize=(8, 6))